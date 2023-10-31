8f5a88c1337638a1abbaa431bb2d9a75e350a7bd
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 16:10:57 +0300

    betty issue fixed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_tree_is_leaf - Checks if node is a leaf
- * node: Pointer to the node to check
+ * @node: Pointer to the node to check
  * Return: 1 if leaf, otherwise 0
 */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-        if (node)
-        {
-                if (!(node->left || node->right))
-                        return 1;
-        }
-        return 0;
+	if (node)
+	{
+		if (!(node->left || node->right))
+		return 1;
+	}
+	return (0);
 }
