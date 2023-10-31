ef55382d194f86b4ad3935f97a301af0fbc97fdd
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 21:12:15 +0300

    task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Goes through a binary tree using preorder traversal
+ * @tree: Pointer to the root
+ * @func: Pointer to a function to call for each node.
+*/
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left);
+		binary_tree_preorder(tree->right);
+	}
+}
