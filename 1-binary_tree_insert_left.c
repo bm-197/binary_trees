ab23d0f9be286eb1afea4f3e6d865952ede4aeea
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 15:55:42 +0300

    indentation issue for task 1 fixed

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,21 +8,21 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *node = NULL;
-
-        node = malloc(sizeof(binary_tree_t));
-        if (parent)
-        {
-                if (node)
-                {
-                        node = binary_tree_node(parent, value);
-                        if (parent->left)
-                        {
-                                node->left = parent->left;
-                                parent->left->parent = node;
-                        }
-                        parent->left = node;
-                }
-        }
-        return node;
+	binary_tree_t *node = NULL;
+	
+	node = malloc(sizeof(binary_tree_t));
+	if (parent)
+	{
+		if (node)
+		{
+			node = binary_tree_node(parent, value);
+			if (parent->left)
+			{
+				node->left = parent->left;
+				parent->left->parent = node;
+			}
+			parent->left = node;
+		}
+	}
+	return node;
 }
