12b4e8810bc59225f0f0a76735a06f2962ca608f
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 14:26:22 +0300

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/*
+ * binary_tree_node - Creates a binary tree node.
+ * parent: Parent node.
+ * value: Value at node.
+ * Return: A binary tree
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+        binary_tree_t *node = NULL;
+
+        node = malloc(sizeof(binary_tree_t));
+        if (node)
+        {
+                node->parent = parent;
+                node->n = value;
+                node->left = NULL;
+                node->right = NULL;
+        }
+        return node;
+}
