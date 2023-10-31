382ae886069aeb28141cf59412ebd008f79ef9a7
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 15:24:51 +0300

    task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree.
+ * tree: Pointer to the root
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+        if (tree)
+        {
+                binary_tree_delete(tree->left);
+                binary_tree_delete(tree->right);
+                free(tree);
+        }
+}
