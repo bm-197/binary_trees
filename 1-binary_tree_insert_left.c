e9b5cf930b0341977df71156b81f7e820d03e16e
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 16:18:25 +0300

    task 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
-	node = malloc(sizeof(binary_tree_t));
 	if (parent)
 	{
 		node = binary_tree_node(parent, value);
