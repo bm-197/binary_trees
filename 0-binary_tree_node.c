7a9b75c8d817d60f6726dea412851003a2eff952
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-10-31 15:59:15 +0300

    indetaion issue for task 0 fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
-	
+
 	node = malloc(sizeof(binary_tree_t));
 	if (node)
 	{
@@ -19,5 +19,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		node->left = NULL;
 		node->right = NULL;
 	}
-	return node;
+	return (node);
 }
