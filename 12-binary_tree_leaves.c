fcce8291e4f38091d888adb593f09eb5bddc2c3e
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-01 12:16:23 +0300

    task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node
+ * Return: 0 if tree is NULL, number of leaves otherwise
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (tree->right == NULL && tree->left == NULL)
+			return (1);
+		
+		return(binary_tree_leaves(tree->left) +
+		binary_tree_leaves(tree->right));
+	}
+	retrun (0);
+}
