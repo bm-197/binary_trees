b0009c5eee44d3939b7231f7110ba5ebbd8a6ba3
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-02 23:53:31 +0300

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,20 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		if (binary_tree_leaves(tree))
-			return (0);
+	int r1 = 0;
+	int r2 = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	}
+
+	r1 = binary_tree_is_full(tree->left);
+	r2 = binary_tree_is_full(tree->right);
+
+	if (r1 && r2)
+		return (1);
+
 	return (0);
 }
