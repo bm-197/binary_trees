312a0d0cf0fe35d5e5a5d1ef818633f85660f946
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-03 18:34:24 +0300

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,13 +3,13 @@
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree.
  * @tree: A pointer to the root node of the tree to measure the balance factor.
- * Return: If tree is NULL, return 0, else return balance factor. 
+ * Return: If tree is NULL, return 0, else return balance factor.
 */
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
 		return (binary_tree_height(tree->left) - binary_tree_hieght(tree->right));
-	
+
 	return (0);
 }
