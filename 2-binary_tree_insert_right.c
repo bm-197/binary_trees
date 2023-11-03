fb5b1c6dddf7335e71c604a774e7205302ecc4ac
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-03 18:22:19 +0300

    task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		node = binary_tree_node(parent, value);
 		if (node)
-		{	
+		{
 			if (parent->right)
 			{
 				node->right = parent->right;
