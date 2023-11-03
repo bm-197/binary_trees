0f325784cee3c7e5e113575683fb811133894c01
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-03 18:30:12 +0300

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,8 +12,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		if (tree->right == NULL && tree->left == NULL)
 			return (1);
-		
-		return(binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 	}
 	return (0);
 }
