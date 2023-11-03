c52450ebd210701082f8bfd439df11be2a56a81a
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-03 18:32:17 +0300

    task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,11 +15,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		if (tree->left && tree->right)
 			node += 1;
 
-		if (tree->left && tree->right)
-		{
-			node += binary_tree_nodes(tree->left);
-			node += binary_tree_nodes(tree->right);
-		}
+		node += binary_tree_nodes(tree->left);
+		node += binary_tree_nodes(tree->right);
 	}
 	return (node);
 }
