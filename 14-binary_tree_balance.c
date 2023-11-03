090b8b71ac1bd2af4e3439530ede6276bd7193b1
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2023-11-03 18:40:40 +0300

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,3 +13,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root
+ * 
+ * Return: the height, 0 if tree is NULL
+*/
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) > binary_tree_height(tree->right))
+		return (binary_tree_height(tree->left) + 1);
+	else
+		return (binary_tree_height(tree->right) + 1);
+
+	return (-1);
+}
